f51674bbf0b0d5561985f5846df272aa7acc0276
Billy Frock <iam@tdlive.me>
2014-07-28 23:54:41 -0400

    Add to cloneTestModules.sh

--- cloneTestModules.sh
+++ cloneTestModules.sh
@@ -2,5 +2,7 @@
 
 # modules to test
 
-git clone https://github.com/NoMoreSugar/iwritelists-basicbot
-git clone https://github.com/NoMoreSugar/iwritelists-admin
+git clone https://github.com/NoMoreSugar/iwritelists-basicbot ./plugins/iwritelists-basicbot
+git clone https://github.com/NoMoreSugar/iwritelists-admin ./plugins/iwritelists-admin
+
+echo "Done installing modules..."
